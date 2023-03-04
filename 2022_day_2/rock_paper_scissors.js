ba2bda3a3f6533326d144a667e914e9ba8739f38
Maria Juravic <mariajuravic@yahoo.com>
2023-03-04 14:58:43 -0800

    completed day 2 part 2

--- 2022_day_2/rock_paper_scissors.js
+++ 2022_day_2/rock_paper_scissors.js
@@ -10,6 +10,7 @@
 //(1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round
 //(0 if you lost, 3 if the round was a draw, and 6 if you won).
 
+//round1
 // Tie AX, BY, CZ = 6 points
 // Win AY, BZ, CX = 8 points
 //Lose AZ, BX, CY = 1 point
@@ -24,7 +25,7 @@ async function readFile(file) {
   } catch(err) {
     process.exit(1);
   }
-
+  //console.log(result);
   return result;
 }
 
@@ -33,44 +34,33 @@ async function readFile(file) {
 async function calcScore(rounds) {
 
   let roundResults = await readFile(rounds);
-  // console.log("roundResults", roundResults);
+  //console.log("roundResults", roundResults);
 
   let score = 0;
 
   for(let round of roundResults) {
-    if(round === "A X" || round === "B Y" || round === "C Z") {
+    if(round === "A X" || round === "C Y" || round === "B Z") {
       // console.log("score of 6");
       score += 3;
-    } if (round === "A Y" || round === "B Z" || round === "C X") {
+    } if (round === "C X" || round === "B Y" || round === "A Z") {
       // console.log("score of 8");
-      score += 6;
-    } if (round === "A Z" || round === "B X" || round === "C Y") {
+      score += 2;
+    } if (round === "B X" || round === "A Y" || round === "C Z") {
       // console.log("score of 1");
-      score += 0;
-    } if (round[2] === "X") {
       score += 1;
     } if (round[2] === "Y") {
-      score += 2;
-    } if (round[2] === "Z") {
       score += 3;
+    } if (round[2] === "Z") {
+      score += 6;
     }
   }
 
-  //answer 14531
+  //answer round 1 14531
+  //answer round2 11258
   console.log("score", score);
   return score;
 }
 
 calcScore("input.txt")
 
-//result [
-  // 'AY', 'CY', 'CX', 'AY', 'CX', 'CY', 'BZ', 'AY', 'AY', 'CZ',
-  // 'AY', 'AY', 'AY', 'CY', 'AY', 'AY', 'AY', 'AX', 'BX', 'AX',
-  // 'AY', 'CY', 'CX', 'AY', 'AY', 'CY', 'AX', 'AY', 'BZ', 'AY',
-  // 'CX', 'AY', 'AY', 'CZ', 'CY', 'CX', 'AY', 'AZ', 'AY', 'AY',
-  // 'AY', 'AY', 'CY', 'CY', 'BZ', 'BZ', 'CY', 'BX', 'AY', 'AY',
-  // 'AY', 'CZ', 'AY', 'CX', 'AZ', 'CY', 'AY', 'AY', 'AY', 'AY',
-  // 'AY', 'CX', 'AY', 'CY', 'AY', 'CY', 'AY', 'AX', 'CY', 'AX',
-  // 'AY', 'CY', 'AY', 'CX', 'AY', 'AY', 'AY', 'CX', 'AY', 'AY',
-  // 'AY', 'CX', 'AY', 'CX', 'AY', 'AY', 'CZ', 'AY', 'AY', 'AY',
-  // 'CX', 'AY', 'AX', 'CY', 'AX', 'BZ', 'CY', 'AY', 'AY', '_',
+
